52c44f7742cbbd5b485bf019dbbcf0ec6a79f9e1
MaxMozgunov <mozgunov162@gmail.com>
2023-06-19 18:38:44 +0200

    add def talk

--- J.A.R.V.I.S..py
+++ J.A.R.V.I.S..py
@@ -1,1 +1,18 @@
 import speech_recognition as sr
+import os
+import sys
+import pyttsx3
+
+
+
+
+
+def talk(words):
+    print(words)
+    engine = pyttsx3.init()
+    engine.say(words)
+    engine.runAndWait()
+    # os.system("say " + words)
+
+
+talk("Hi, how can I help you?")
