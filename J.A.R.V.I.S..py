c0ef105a38849ee883a81984d0b0891788c70660
MaxMozgunov <mozgunov162@gmail.com>
2023-06-22 18:36:43 +0200

    add task

--- J.A.R.V.I.S..py
+++ J.A.R.V.I.S..py
@@ -1,10 +1,9 @@
+import webbrowser
+
 import speech_recognition as sr
 import os
 import sys
 import pyttsx3
-
-
-
 
 
 def talk(words):
@@ -16,3 +15,29 @@ def talk(words):
 
 
 talk("Hi, how can I help you?")
+
+
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+        try:
+            task = r.recognize_google(audio, language="ua-UA").lower()
+            print("Ви проговорили: " + task)
+        except sr.UnknownValueError:
+            talk("Я вас не зрозумів")
+            task = command()
+        return task
+
+def make_something(task):
+    if "відкрий сайт" in task:
+        talk("Відкриваю")
+        url = "https://ituniver.com"
+        webbrowser.open(url)
+
+
+while True:
+    make_something(command())
